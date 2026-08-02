506a6edfc99b9ee00e96e5d3937fc2ebc1638a3d
Timothy-John-Kaharian <timothyjohnkaharian@gmail.com>
2026-08-02 21:31:54 +0800

    Create student_introduction.py

---
+++ student_introduction.py
@@ -0,0 +1,11 @@
+name = "Timothy John R. Kaharian"
+course = "BSCS - Computer Science"
+section = "3B"
+favorite_AI_technology = "Machine Learning"
+future_AI_project = "Adaptive AI NPC for Video Games"
+
+print(f"\nName: {name}")
+print(f"Course: {course}")
+print(f"Section: {section}")
+print(f"Favorite AI Technology: {favorite_AI_technology}")
+print(f"Future AI Project: {future_AI_project} \n")
